dcf5de9752d266fabd6f0ea13f6580b676f9a18a
thomas <destvincent@hotmail.fr>
2020-12-15 14:14:09 +0100

    fetcherror

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,31 @@
 import useSWR from 'swr'
 import FetchError from './FetchError'
 
+const responseBody = (response) => {
+  const contentType = response.headers.get("content-type");
+  if (contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json();
+  }
+  return response.text()
+}
+
 const fetcher = (resource, init = {}) => {
   const headers = new Headers(init.headers || {})
   const myInit = {
     method: 'GET',
+    mode: 'cors',
     ...init,
     headers
   }
   return fetch(resource, myInit).then((response) => {
-    if (response.ok) {
-      return response.json()
+    const contentType = response.headers.get("content-type");
+    if (!response.ok) {
+      console.log('response', response)
+      throw new FetchError(response)
+    }
+
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+      return response.json();
     }
 
     return response
@@ -19,9 +34,18 @@ const fetcher = (resource, init = {}) => {
         throw new FetchError(response)
       })
       .then((text) => {
-        console.log('text', text)
         throw new FetchError(response, text)
       })
+  }).catch(error => {
+    if (error instanceof FetchError) {
+      throw error
+    }
+    throw new FetchError({
+      type: myInit.method,
+      url: resource,
+      status: -1,
+      statusText: error.message
+    })
   })
 }
 
